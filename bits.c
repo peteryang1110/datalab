93d3d8c15f18f3d8ef056fd085a8e0e220c61980
Yun-Ping Yang <yuya0723@colorado.edu>
2019-08-26 13:54:49 -0600

    put the name and userid

--- bits.c
+++ bits.c
@@ -2,6 +2,7 @@
  * CS:APP Data Lab 
  * 
  * <Please put your name and userid here>
+ * Yun-Ping Yang, yuya0723
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
